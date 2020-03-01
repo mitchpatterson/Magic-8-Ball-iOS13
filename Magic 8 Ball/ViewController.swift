70c27e0f81eab56b6baa6bb3513cd0f23883a53c
mitchpatterson <mpatterson12@live.ca>
2020-03-01 10:02:28 -0500

    Creating and randomizing response to ask

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
 
 
-
+    @IBOutlet weak var responseToQustion: UIImageView!
+    
+    @IBAction func askButtonPressed(_ sender: Any) {
+        responseToQustion.image = ballArray.randomElement()
+    }
 }
 
